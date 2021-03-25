07c1aee39d1e882bb3cee272f6e834037b18ace9
Konstantin Dmitrenko <werhawer@gmail.com>
2021-03-25 12:30:02 +0200

    try to import create todo form

--- src/cljs/test_proj/views.cljs
+++ src/cljs/test_proj/views.cljs
@@ -6,8 +6,9 @@
    [test-proj.events :as events]
    [nano-id.core :refer [nano-id]]
    [test-proj.components.atoms.button.core :refer [button]]
-   [test-proj.components.atoms.input.core :refer [input]]))
-  ;;  [test-proj.components.moleculs.forms.create-todo.core :refer [create-todo-form]]
+   [test-proj.components.atoms.input.core :refer [input]]
+   [test-proj.components.moleculs.forms.create-todo.core :refer [create-todo-form]]))
+   
    
    
 
@@ -68,37 +69,37 @@
 ;; CREATE-TODO FORM ---------------------------
 ;; 
 
-(defn new-todo-generator []
-  (let [title (rf/subscribe [:create-form-title])
-        text (rf/subscribe [:create-form-text])
-        priority (rf/subscribe [:create-form-priority])]
+;; (defn new-todo-generator []
+;;   (let [title (rf/subscribe [:create-form-title])
+;;         text (rf/subscribe [:create-form-text])
+;;         priority (rf/subscribe [:create-form-priority])]
 
-    {:title @title :text @text :priority @priority :id (nano-id) :done? false}))
+;;     {:title @title :text @text :priority @priority :id (nano-id) :done? false}))
 
 
-(defn on-create-form-submit [e]
- (let [title (rf/subscribe [:create-form-title])
-        text (rf/subscribe [:create-form-text])]
+;; (defn on-create-form-submit [e]
+;;  (let [title (rf/subscribe [:create-form-title])
+;;         text (rf/subscribe [:create-form-text])]
        
-   (.preventDefault e)
-   (if (and (not-empty? @title) (not-empty? @text))
-     (rf/dispatch [::events/add-new-todo (new-todo-generator)]))))
+;;    (.preventDefault e)
+;;    (if (and (not-empty? @title) (not-empty? @text))
+;;      (rf/dispatch [::events/add-new-todo (new-todo-generator)]))))
 
 
-(defn create-todo-form []
-  (let [title (rf/subscribe [:create-form-title])
-        text (rf/subscribe [:create-form-text])
-        priority (rf/subscribe [:create-form-priority])]
+;; (defn create-todo-form []
+;;   (let [title (rf/subscribe [:create-form-title])
+;;         text (rf/subscribe [:create-form-text])
+;;         priority (rf/subscribe [:create-form-priority])]
     
-     [:form {:on-submit on-create-form-submit :class "create-form"}
-      [:h3 "Create todo"]
-      (button {:on-click close-modal :icon "zmdi-close" :size :smaller :class "close-modal-btn" :variant "circle"})
+;;      [:form {:on-submit on-create-form-submit :class "create-form"}
+;;       [:h3 "Create todo"]
+;;       (button {:on-click close-modal :icon "zmdi-close" :size :smaller :class "close-modal-btn" :variant "circle"})
 
-      (input {:variant "text" :value @title :on-change #(rf/dispatch [:new-todo-title %])})
+;;       (input {:variant "text" :value @title :on-change #(rf/dispatch [:new-todo-title %])})
 
-      (input {:variant "textarea" :value @text :on-change #(rf/dispatch [::events/new-todo-text %])})
+;;       (input {:variant "textarea" :value @text :on-change #(rf/dispatch [::events/new-todo-text %])})
 
-      (button {:type "submit" :variant "simple"})]))
+;;       (button {:type "submit" :variant "simple"})]))
 
 ;; MODAL -------------------------------
 
